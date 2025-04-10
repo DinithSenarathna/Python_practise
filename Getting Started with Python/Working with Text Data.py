e9c005999f865304e1096023ffd3bf939c55d042
Dinith Senarathna <116188963+DinithSenarathna@users.noreply.github.com>
2025-04-10 06:08:49 +0530

    Update Working with Text Data.py

--- Getting Started with Python/Working with Text Data.py
+++ Getting Started with Python/Working with Text Data.py
@@ -1,1 +1,13 @@
+#Text vs Numbers
+print(2.71)
+print("October")
 
+#String Data Type
+print("I am a string")
+
+#String Concatenation
+print("Hello" + " " + "World")
+print("Theme " + "Park")
+
+#Strings
+print("28"+"6")
