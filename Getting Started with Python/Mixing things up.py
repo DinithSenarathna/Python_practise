f31a9cd1fbbcfb86e6f40c59c3fded1c37ca131b
Dinith Senarathna <116188963+DinithSenarathna@users.noreply.github.com>
2025-04-10 06:12:43 +0530

    Update Mixing things up.py

--- Getting Started with Python/Mixing things up.py
+++ Getting Started with Python/Mixing things up.py
@@ -1,1 +1,13 @@
+#Multiple Lines
+print("Name:")
+print("Mary")
+print("Balance:")
+print(500)
+
+#Multiple Outputs
+print("Balance:", 5000)
+
+#Output Separators
+print("Iron", "Man", sep ="-")
+
 
