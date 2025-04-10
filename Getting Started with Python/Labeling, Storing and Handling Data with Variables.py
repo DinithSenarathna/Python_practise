80c6275ef293c1091ab5f8580c4bea3bdf35d04a
Dinith Senarathna <116188963+DinithSenarathna@users.noreply.github.com>
2025-04-10 06:17:46 +0530

    Update Labeling, Storing and Handling Data with Variables.py

--- Getting Started with Python/Labeling, Storing and Handling Data with Variables.py
+++ Getting Started with Python/Labeling, Storing and Handling Data with Variables.py
@@ -1,1 +1,36 @@
+#Using variables
+points=15
+print(points)
+
+#Assign to Variable
+x = 42       #integer
+y = "Hello"  #string
+z = 3.14	 #float
+
+print(x)
+print(y)
+print(z)
+
+#Working with Variables
+price= 15
+print(price*3)
+
+#Reassigning Variables
+x = 8
+print(x)
+x = 41
+print(x)
+
+#Operations with Variables
+x = 9
+y = 5
+
+print(x*y)
+print(x/y)
+
+#Output
+result = 798
+currency = "$"
+print("Total:", currency, result)
+
 
